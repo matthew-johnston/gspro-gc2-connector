1bca22c5865ef380a18b38c111ff26e885983280
Matthew Johnston <matcric@gmail.com>
2024-07-24 21:58:32 +1200

    WIP the connection still seems broken (not sure why)

--- src/main.rs
+++ src/main.rs
@@ -21,34 +21,57 @@ fn main() {
     let port_name = args.port;
     let baud_rate = args.baud;
 
-    let port = serialport::new(&port_name, baud_rate)
-        .timeout(Duration::from_millis(10_000))
-        .open();
+    // let port = serialport::new(&port_name, baud_rate)
+    //     .timeout(Duration::from_millis(10))
+    //     .open();
 
-    match port {
-        Ok(mut port) => {
-            let mut serial_buf: Vec<u8> = vec![0; 1000];
-            println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
-            loop {
-                match port.read(serial_buf.as_mut_slice()) {
-                    Ok(t) => {
-                        let data = String::from_utf8_lossy(&serial_buf[..t]);
-                        if data.starts_with("CT") {
-                            let ball_event = ball_event::BallEvent::from_data_line(&data);
-                            println!("{:?}", ball_event);
-                        }
+    let mut port = open_serial_port(&port_name, baud_rate);
 
-                        io::stdout().write_all(&serial_buf[..t]).unwrap();
-                        io::stdout().flush().unwrap();
-                    }
-                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-                    Err(e) => eprintln!("{:?}", e),
-                }
+    let mut serial_buf: Vec<u8> = vec![0; 1000];
+    println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
+    loop {
+        match port.read(serial_buf.as_mut_slice()) {
+            Ok(t) => {
+                // let data = String::from_utf8_lossy(&serial_buf[..t]);
+                // if data.starts_with("CT") {
+                //     let ball_event = ball_event::BallEvent::from_data_line(&data);
+                //     println!("{:?}", ball_event);
+                // }
+
+                io::stdout().write_all(&serial_buf[..t]).unwrap();
+                io::stdout().flush().unwrap();
             }
-        }
-        Err(e) => {
-            eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
-            ::std::process::exit(1);
+            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+            Err(e) => eprintln!("{:?}", e),
         }
     }
+}
+
+fn open_serial_port(port_name: &str, baud_rate: u32) -> Box<dyn serialport::SerialPort> {
+    loop {
+        match serialport::new(port_name, baud_rate)
+            .timeout(Duration::from_millis(10))
+            .stop_bits(serialport::StopBits::One)
+            .parity(serialport::Parity::None)
+            .data_bits(serialport::DataBits::Eight)
+            .open()
+        {
+            Ok(p) => return p,
+            Err(e) => {
+                eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
+                // ::std::process::exit(1);
+            }
+        }
+    }
+    // let port = serialport::new(port_name, baud_rate)
+    //     .timeout(Duration::from_millis(10))
+    //     .open();
+
+    // match port {
+    //     Ok(p) => p,
+    //     Err(e) => {
+    //         eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
+    //         ::std::process::exit(1);
+    //     }
+    // }
 }
